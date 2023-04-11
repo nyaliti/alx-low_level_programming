7a6be8b44a83b4d89b06c7f6b47ba7538af8b6b8
nyaliti <bnyaliti@gmail.com>
2023-04-10 17:30:29 -0700

    updated 0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,36 @@
 #include "main.h"
 
 /**
- * create_file - creates a file.
- * @filename: The file name
- * @text_content: The text to be written to the new file
+ * create_new_file - creates a new file and writes text to it
+ * @filename: name of the file to create
+ * @text_content: text to write to the file
  *
- * Return: (int)
+ * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int create_new_file(const char *filename, char *text_content)
 {
+    int file_des, status = -1;
+    int text_length, x;
+
     if (filename == NULL)
-        return (-1);
+        return (status);
 
-    int file_des = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (file_des == -1)
-        return (-1);
+    file_des = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    ssize_t bytes_written = 0;
-    if (text_content != NULL) {
-        bytes_written = write(file_des, text_content, strlen(text_content));
-        if (bytes_written == -1) {
-            close(file_des);
-            return (-1);
-        }
+    if (file_des < 0)
+        return (status);
+
+    status = 1;
+    if (text_content != NULL)
+    {
+        for (i = 0, text_length = 0; text_content[x] != '\0'; x++)
+            text_length++;
+        status = write(file_des, text_content, text_length);
+        if (status >= 0)
+            status = 1;
     }
 
     close(file_des);
-    return (bytes_written == -1 ? -1 : 1);
+    return (status);
 }
-
 

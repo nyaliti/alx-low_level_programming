e62f832b5b63443e809f43dc5cf0180b7ab4a997
nyaliti <bnyaliti@gmail.com>
2023-04-20 02:41:36 -0700

    updated 0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,29 +9,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;  // file descriptor
-	int bytes_written, i = 0;  // bytes_written - number of bytes written to the file, i - loop counter
+    int fd, x, y = 0;
 
-	if (!filename)  // check if filename is NULL
-		return (-1);
+    if (!filename) // check if filename is null
+        return (-1);
 
-	// create the file with read, write, and execute permissions for owner, and no permissions for group and others
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600); 
-	if (fd < 0)  // check if file descriptor is valid
-		return (-1);
+    // create a new file with read/write permission for owner only
+    // and truncate it if it already exists
+    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+    if (fd < 0) // check if open failed
+        return (-1);
 
-	if (text_content)  // if there is text to write
-	{
-		// count the number of characters in the text content
-		for (; text_content[i] != '\0'; i++)
-			;
-		bytes_written = write(fd, text_content, i);  // write the text content to the file
-		if (bytes_written != i)  // check if all bytes were written
-			return (-1);
-	}
+    if (text_content) // check if there is text to write
+    {
+        while (text_content[y]) // count the number of characters in the text
+            y++;
 
-	close(fd);  // close the file descriptor
+        // write the text to the file
+        x = write(fd, text_content, y);
+        if (x != y) // check if the write was incomplete
+            return (-1);
+    }
 
-	return (1);
+    close(fd); // close the file
+
+    return (1); // return success
 }
 
